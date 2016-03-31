b2a1560be84a5bf5569775f885871f9a312bff6f
Ben Wiederhake <BenWiederhake.GitHub@gmx.de>
2016-03-31 20:37:27 +0200

    First somewhat running version

---
+++ Makefile
@@ -0,0 +1,33 @@
+# ohno - Interpreter for the impossibly difficult language "ohno"
+# Copyright 2016 Ben Wiederhake
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+all = bin
+
+.PHONY: bin
+bin: ohno
+
+libkeccak/Makefile:
+	@echo "You didn't use '--recursive' when cloning, did you?"
+	@echo "Try 'git submodule update --init' to fix this."
+# The great thing about 'false' is that it does it's job,
+# even if it's not installed!  ;-)
+	@false
+
+libkeccak/bin/libkeccak.a: libkeccak/Makefile
+	+make -C libkeccak
+
+ohno: ohno.c libkeccak/bin/libkeccak.a
+	gcc -Wall -Wextra -pedantic -Werror -g -O2 -std=c99 -o $@ $<
---
+++ ohno.c
@@ -0,0 +1,183 @@
+/*
+ ohno - Interpreter for the impossibly difficult language "ohno"
+ Copyright 2016 Ben Wiederhake
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+ --
+
+ Compile like this:
+   make ohno
+ Run like this:
+   ./ohno <path/to/your/program>
+
+ As it is impossibly hard to write a meaningful program in ohno,
+ I'm not sure whether there are any bugs.
+
+ For an explanation how this (should) work, see README.md
+
+ This project started at https://github.com/BenWiederhake/ohno
+ Feel free to leave me some feedback, or even a Pull Request! :D
+ */
+
+#include <assert.h>
+
+#ifndef __USE_MISC
+#define __USE_MISC
+#endif
+#include <endian.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+
+const char* const USAGE =
+"Usage:\n"
+"  %s <path/to/your/program>\n"
+"\n"
+"Your program should be written ohno, that is:\n"
+"- At least 4 bytes long\n"
+"- The first 4 bytes are (in little endian) the length of the ohno bytecode\n"
+"- The rest of the file may be anything\n"
+"\n"
+"The ohno bytecode is computed by computing the SHA3-512 of the entire input\n"
+"file, and essentially interpreting that hash as brainfuck instructions.\n"
+"\n"
+"For more information about how to write ohno code, please read\n"
+"the documentation.\n";
+
+#ifndef OHNO_SIZE_T_MODIFIER
+#define OHNO_SIZE_T_MODIFIER "ld"
+#endif
+
+typedef struct chunk {
+    size_t len;
+    unsigned char* data;
+} chunk;
+
+chunk read_to_file(const char* filename) {
+    chunk chk;
+    FILE* fp = fopen(filename, "rb");
+    if (!fp) {
+        perror("Couldn't open file");
+        chk.len = 0;
+        return chk;
+    }
+    if (fseek(fp, 0, SEEK_END)) {
+        perror("Problem while reading (fseek)");
+        fclose(fp);
+        chk.len = 0;
+        return chk;
+    }
+    /* This ignores the glaringly obvious race condition:
+     * What if someone writes to 'fp' after this? */
+    long int raw_len = ftell(fp);
+    if (raw_len < 0) {
+        perror("Problem while reading (ftell)");
+        fclose(fp);
+        chk.len = 0;
+        return chk;
+    }
+    chk.len = (size_t)raw_len;
+    if (chk.len < 8) {
+        perror("File is too short");
+        fclose(fp);
+        chk.len = 0;
+        return chk;
+    }
+    if (fseek(fp, 0, SEEK_SET)) {
+        perror("Problem while reading (fseek back)");
+        fclose(fp);
+        chk.len = 0;
+        return chk;
+    }
+
+    chk.data = malloc(chk.len);
+    if (!chk.data) {
+        fprintf(stderr, "Bad malloc for size %" OHNO_SIZE_T_MODIFIER "?!\n", chk.len);
+        fclose(fp);
+        chk.len = 0;
+        return chk;
+    }
+
+    if (1 != fread(chk.data, chk.len, 1, fp)) {
+        fprintf(stderr, "Problem while reading %" OHNO_SIZE_T_MODIFIER " bytes from %s\n", chk.len,
+                filename);
+        fclose(fp);
+        chk.len = 0;
+        free(chk.data);
+        chk.data = NULL;
+        return chk;
+    }
+
+    fclose(fp);
+
+    /* Whew! Finally. */
+    return chk;
+}
+
+//int to_sha3(chunk chk_raw, chunk chk_sha3) {
+//
+//}
+
+//typedef struct bf_chunk {
+//    unsigned char content[4096 - 2 * sizeof(void*)];
+//    bf_chunk* left;
+//    bf_chunk* right;
+//} bf_chunk;
+
+//int run_bf(chunk* code, bf_chunk* tape) {
+//
+//}
+
+int main(const int argc, const char **argv) {
+    if (argc != 2) {
+        fprintf(stderr, USAGE, argv[0]);
+        return 1;
+    }
+
+    /* Read file */
+    chunk content = read_to_file(argv[1]);
+    if (content.len < 4) {
+        return 1;
+    }
+
+    /* Compute sha3 */
+    typedef char check_uint_size[(sizeof(unsigned int) >= 4) ? 1 : -1];
+    assert(sizeof(check_uint_size) > 0);
+    chunk sha3;
+    sha3.len = le64toh(*(unsigned int* )content.data);
+    fprintf(stderr, "That'll be %" OHNO_SIZE_T_MODIFIER " bytes of brainfuck.\n", sha3.len);
+    sha3.data = malloc(sha3.len);
+    if (!sha3.data) {
+        fprintf(stderr, "Couldn't allocate that much.\n");
+        free(content.data);
+        return 1;
+    }
+
+    fprintf(stderr, "So far, so good.\n");
+    return 0;
+//    if (!to_sha3(content, sha3)) {
+//        fprintf(stderr, "Couldn't compute SHA3 ... huh?\n");
+//        free(content.data);
+//        free(sha3.data);
+//        return 1;
+//    }
+//
+//    bf_chunk tape;
+//    memset(tape.content, 0, sizeof(tape.content));
+//    tape.left = NULL;
+//    tape.right = NULL;
+//
+//    return run_bf(sha3, &tape);
+}
